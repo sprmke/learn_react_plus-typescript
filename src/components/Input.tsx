acc82dd60ff8bcf7957f0e0ee57a87f16b2036ab
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-08-26 05:15:16 +0800

    8. prop types & tips

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,12 @@ type InputProps = {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Input = (props: InputProps) => {
+export const Input = ({ value, handleChange }: InputProps) => {
   return (
     <input
       type='text'
-      value={props.value}
-      onChange={(event) => props.handleChange(event)}
+      value={value}
+      onChange={(event) => handleChange(event)}
     />
   );
 };
