fdc907ac00bc0d05aa2dc2fe8fb80d43deba2d57
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-08-26 02:18:29 +0800

    4. basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Michael',
+    last: 'Manlulu',
+  };
+
+  const nameList = [
+    { first: 'Michael', last: 'Manlulu' },
+    { first: 'Mikki', last: 'Manlulu' },
+    { first: 'Mateo', last: 'Manlulu' },
+  ];
+
   return (
     <div className='App'>
-      <Greet name='Mike' />
+      <Greet name='Mike' messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
