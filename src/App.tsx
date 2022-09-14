315265c7600a0099a9ae96675a1d9c50410006f5
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-14 08:56:45 +0800

    18. component prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css';
-import { UserContextProvider } from './components/context/UserContext';
-import { Counter } from './components/class/Counter';
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
 
 function App() {
   return (
     <div className='App'>
-      <UserContextProvider>
-        <Counter message={'The count value is'} />
-      </UserContextProvider>
+      <Private isLoggedIn={true} component={Profile} />
     </div>
   );
 }
